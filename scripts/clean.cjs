0e85bf2451dae6efead466ef73b02b435b21a86d
brianleroux <b@brian.io>
2022-07-26 22:28:35 -0700

    updated vendoring

--- scripts/clean.cjs
+++ scripts/clean.cjs
@@ -2,5 +2,9 @@ let path = require('path')
 let fs = require('fs')
 let pathToPkg = path.join(__dirname, '..', 'vendor', 'package.json')
 let pkg = require(pathToPkg)
+
+delete pkg.name
+delete pkg.version
 delete pkg.scripts.postinstall
+
 fs.writeFileSync(pathToPkg, JSON.stringify(pkg, null, 2))
--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -4,4 +4,5 @@ cp -r node_modules/@enhance/starter-project vendor
 rm -rf vendor/.github 
 rm -rf vendor/scripts
 rm -rf vendor/manifest.json
+rm vendor/readme.md
 node scripts/clean.cjs
