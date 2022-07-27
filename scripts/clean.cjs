7f0d18a9c6f675fd49aff29b025922317d82d8fc
brian <brian@brians-MacBook-Air.local>
2022-07-26 18:14:29 -0700

    remove postinstall from generated proj

--- scripts/clean.cjs
+++ scripts/clean.cjs
@@ -2,5 +2,5 @@ let path = require('path')
 let fs = require('fs')
 let pathToPkg = path.join(__dirname, '..', 'vendor', 'package.json')
 let pkg = require(pathToPkg)
-delete pkg.postinstall
+delete pkg.scripts.postinstall
 fs.writeFileSync(pathToPkg, JSON.stringify(pkg, null, 2))
