8a29fa459873705adadbe5c395eecf7ceaf6a452
Taylor Beseda <tbeseda@gmail.com>
2022-12-19 11:38:38 -0700

    Update vendor-template.sh

--- scripts/vendor-template.sh
+++ scripts/vendor-template.sh
@@ -4,10 +4,6 @@ rm -rf template # in case it exists
 cp -r node_modules/@enhance/starter-project template
 
 # Remove files/folders we don't need
-rm -rf template/.github/  # TODO: remove
-rm -rf template/scripts/  # TODO: remove
-rm template/manifest.json # TODO: remove
-rm template/CHANGELOG.md  # TODO: remove
 rm template/readme.md
 rm template/LICENSE
 
@@ -20,8 +16,8 @@ const pkg = require('./template/package.json')
 delete pkg.name
 delete pkg.version
 delete pkg.scripts.postinstall
-JSON.stringify(pkg, null, 2)" > template/new-package
+JSON.stringify(pkg, null, 2)" > template/new-package.json
 
-mv template/new-package template/package.json
+mv template/new-package.json template/package.json
 
 echo "Vendored starter project into template folder"
